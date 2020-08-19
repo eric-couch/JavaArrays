448837494e00d7eab66c62eb5ebfaa72831262e8
Eric Couch <ericcouch@yahoo.com>
2020-08-19 10:33:00 -0500

    java switch and methods

--- Main.java
+++ Main.java
@@ -2,62 +2,51 @@ package com.company;
 
 public class Main {
 
+    public static float priceOfShipping(float orderValue,
+                                        boolean saturdayDelivery,
+                                        String destination) {
+        float deliverCost = 0;
+        switch (destination) {
+            case "US":
+                deliverCost = 20.00f;
+                if (orderValue > 100) {
+                    deliverCost -= 5.00f;
+                }
+                if (saturdayDelivery) {
+                    deliverCost += 10.00f;
+                }
+                break;
+            case "Mexico":
+                deliverCost = 32.00f;
+                break;
+            default:
+                deliverCost = 25.00f;
+                if (orderValue > 100) {
+                    deliverCost -= 7.00f;
+                }
+                if (saturdayDelivery) {
+                    deliverCost += 12.00f;
+                }
+                break;
+        }
+
+        return deliverCost;
+    }
+
     public static void main(String[] args) {
-        // mutable vs immutable
-        String name = "James Eric Couch";
-
-        //trim
-        String trimmedName = name.trim();
-
-        //charAt
-        System.out.println(name.charAt(5));
-
-        //contains return true or false if the string contains the parameter string
-        System.out.println(name.contains("Co"));
-
-        //indexOf
-        System.out.println(name.indexOf("x"));
-
-        //replace
-        System.out.println(name.replace("c", "x"));
-
-        //split
-        String[] x = name.split(" ");
-        System.out.println(x[0]);
-        System.out.println(x[1]);
-        System.out.println(x[2]);
-
-        String filePath = "C:/Users/ericc/source/repos/JavaArrays/src/com/company/Main.java";
-        String otherFilePath = "C:/root/subdir/otherMain.java";
-
-        String[] filePathParts = filePath.split("/");
-        String[] otherFilePathParts = otherFilePath.split("/");
-
-        System.out.println(filePathParts[8]);
-        System.out.println(filePathParts[9]);
 
 
-        System.out.println(filePathParts[filePathParts.length-1]);
-        System.out.println(otherFilePathParts[otherFilePathParts.length-1]);
+        System.out.println(priceOfShipping(200, true, "Canada"));
 
-        String speech = "Now is the time for all good men to come to the aid of their country.";
+        float returnFromMethod = priceOfShipping(200, true, "Canada");
+        System.out.println(returnFromMethod);
 
-        // length
-        System.out.println(speech.length());
-        System.out.println(speech.split(" ").length);
+        returnFromMethod = priceOfShipping(100, false, "Mexico");
+        System.out.println(returnFromMethod);
 
-        System.out.println(speech.split(" ")[0]);
+        System.out.println(priceOfShipping(50, true, "US"));
 
-        // toLowerCase / toUpperCase
-        System.out.println(name.toLowerCase());
 
-        // substring
-        System.out.println(speech.substring(20,32) + " " + speech.substring(48));
 
-        // escape characters
-        // Ice Cube says, "Today was a good day."
-        System.out.println("Ice Cube says, \"Today was a good day.\"");
-        System.out.println("Eric is a good teacher \nGary is a good teacher as well");
-        System.out.println("Eric is pretty \tgood teacher");
     }
 }
